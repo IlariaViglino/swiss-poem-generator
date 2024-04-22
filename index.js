c7e568fa3c55f6a5e40232baccd9a57c5a17f99b
IlariaViglino <ilaria.viglino@yahoo.it>
2024-04-22 18:52:16 +0200

    AI API

--- index.js
+++ index.js
@@ -7,20 +7,30 @@ new Typewriter('#poem', {
   });
 
 
-
 let form=document.querySelector("#poem-generator-form");
 
-function poemType(event){
+function generatePoem(event){
     event.preventDefault();
     let instructions=document.querySelector(".instructions");
-    
+
+    let apiKey = "do37btb04e66032f8eb1ab0493255777";
+    let context = "You are a Swiss poets and poem expert, please carefully follow the instructions. Sign the poem with the Author's name inside the <strong></strong> element with the id of signature. Since Switzerland has multiple official languages, your can type the poem in English, German, French, Italian or Romansh, according to the original language of the poem";
+    let prompt = `Write a Swiss poem about the topic: ${instructions.value}?`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+function displayPoem(response){
     new Typewriter('#poem', {
-        strings: `Poem about ${instructions.value}`,
+        strings: `${response.data.answer}`,
         autoStart: true,
         delay: 1,
         cursor: "",
       });
+}
+axios.get(apiUrl).then(displayPoem);
+   
+    
+    
 
 }
 
-form.addEventListener("submit",poemType);
+form.addEventListener("submit",generatePoem);
